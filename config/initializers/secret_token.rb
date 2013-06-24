e498ec8b817c64da920f84445e7fe593c934fc94
Patricio <patricio3d@gmail.com>
2013-06-24 17:24:32 -0500

    prueva 1
    1.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Patricio::Application.config.secret_token = '467b6f00cde3e2b6be2aef046ab51059d30cd67c3bf128f5c4819584e7d379a380ecc51cffcd57a58ce83f863438deb190658e86c424c45b271e1a0264601caa'
